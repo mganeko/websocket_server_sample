3ca4b91ecf9ac46aaca9186ed87a79a970702d27
mganeko <masashi.g@gmail.com>
2022-10-26 23:17:13 +0900

    websocket client

--- websocket_client.js
+++ websocket_client.js
@@ -1,33 +1,33 @@
 const WSClient = require('websocket').client;
 
-var client = new WSClient();
+const client = new WSClient();
 
 client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
+  console.log('Connect Error: ' + error.toString());
 });
 
 client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-    
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            connection.sendUTF(number.toString());
-            setTimeout(sendNumber, 1000);
-        }
+  console.log('WebSocket Client Connected');
+  
+  connection.on('error', function(error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+    console.log('echo-protocol Connection Closed');
+  });
+
+  connection.on('message', function(message) {
+    //console.log('message:', message);
+    if (message.type === 'utf8') {
+       console.log("Received: '" + message.utf8Data + "'");
+       if (message.utf8Data === 'Echoback:Hello') {
+          console.log('got hello');
+          connection.send('QUIT');
+       }
     }
-    sendNumber();
+  });
+
+  connection.send('Hello');
 });
 
 const PORT = 8000;
