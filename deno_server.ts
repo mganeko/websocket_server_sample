b14482cb390a7e1f5a15d872b401243c2676383c
mganeko <masashi.g@gmail.com>
2022-11-16 22:19:24 +0900

    try deno server

---
+++ deno_server.ts
@@ -0,0 +1,22 @@
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
+
+const PORT = 8000;
+
+const wss = new WebSocketServer(PORT);
+wss.on("connection", function (ws: WebSocketClient) {
+  ws.send('connected!');
+
+  ws.on("message", function (message: string) {
+    console.log('received: %s', message);
+    ws.send('Echoback:' + message);
+
+
+    const text = '' + message;
+    if (text === 'QUIT') {
+      console.log('QUIT Server');
+      //await sleep(10); // wait for ws.send() finish
+      process.exit(0);
+      //ws.close(); <-- bun ERROR
+    }
+  });
+});
